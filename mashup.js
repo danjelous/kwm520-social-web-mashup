0e4e07704f8eabc721d6dcb0a2a457e20a2d3667
Danjelous <daniel.brunner@live.at>
2017-05-05 09:53:00 +0200

    Init

---
+++ mashup.js
@@ -0,0 +1,56 @@
+"use-strict";
+
+// Mashup Module (IIF)
+let Mashup = (function WeatherMashup() {
+
+    // Hagenberg
+    let longitude = 14.4996;
+    let latitude = 48.3777;
+
+    let weatherData = {
+        city: document.querySelector('#city'),
+        weather: document.querySelector('#weather'),
+        temperature: document.querySelector('#temperature'),
+        temperatureValue: 0,
+        units: 'K'
+    }
+
+    let getLocationAndWeather = () => {
+
+    }
+
+    let init = () => {
+        
+        try {
+
+            // Does the browser support geolocation?
+            if(typeof(navigator.geolocation) == 'undefined') {
+
+                // Alert <3
+                alert('Sorry, but location services are not supported by your browser!');
+            } else {
+
+                // Get geolocation object
+                let gl = navigator.geolocation;
+                gl.getCurrentPosition(
+
+                    // Success callback
+                    (position) => {
+
+                    },
+
+                    // Error callback
+                    (error) => {
+
+                    }
+                );
+            }
+
+        } catch (error) {
+            
+        }
+    }
+
+    init();
+
+})();
